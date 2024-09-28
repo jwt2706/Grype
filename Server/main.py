046c1e14b13d4af1180c6cd816dd21008e6574a7
jwt2706 <jwt2home@gmail.com>
2024-09-28 14:43:49 -0400

    cleanup

--- Server/main.py
+++ Server/main.py
@@ -4,31 +4,19 @@ import torch
 
 app = Flask(__name__)
 
-goal_categories = ['exercise', 'diet', 'hydration', 'hygiene', 'social', 'sleep', 'production', 'misc']
-sentence_classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 goal_status_pipe = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    input_text = request.json.get("text")
-    sentences = split_sentences(input_text)
-    classified_sentences = []
-    for sentence in sentences:
-        goal_type = classify_sentence(sentence)
-        goal_status = classify_goal_status(sentence)
-        classified_sentences.append({"sentence": sentence, "goal_type": goal_type, "goal_status": goal_status})
-    return classified_sentences
-
-def split_sentences(text):
-    return text.split(".")
-
-def classify_sentence(sentence):
-    result = sentence_classifier(sentence, goal_categories)
-    return result['labels'][0]
+    input_text = request.json.get("text")    
+    goal_status = classify_goal_status(sentence)
+    return jsonify({"goal_status": goal_status})
 
 def classify_goal_status(sentence):
     result = goal_status_pipe(sentence)
-    return result[0]['label']
+    if result[0]['label'] == 'POSITIVE':
+        return 1
+    return 0
 
 if __name__ == "__main__":
     app.run(debug=True)
