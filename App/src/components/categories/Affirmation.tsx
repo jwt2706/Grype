97df727fb1dfff4c49cec8cf42305846585050b5
joeyissa <joeyissa59@gmail.com>
2024-09-28 03:16:08 -0400

    new category enum

--- App/src/components/categories/Affirmation.tsx
+++ App/src/components/categories/Affirmation.tsx
@@ -2,6 +2,11 @@ interface AffirmationProps {
     affirmation: string;
 }
 
+/*
+* handle the category animations within this component
+* use GASP
+*/
+
 const Affirmation = (props: AffirmationProps) => {
     return (
         <div className="text-2xl text-center">
---
+++ App/src/types/Categories.ts
@@ -0,0 +1,6 @@
+export enum CategoryType {
+    EXCERSIZE = "excersize",
+    SOCIAL = "social",
+    FOOD = "food",
+    WATER = "water",
+}
