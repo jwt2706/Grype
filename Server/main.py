05b1184db8efc2997c094c3f7d3f67735d8988e9
jwt2706 <jwt2home@gmail.com>
2024-09-28 01:06:32 -0400

    basic backend

--- Server/main.py
+++ Server/main.py
@@ -23,24 +23,16 @@ def sentiment():
 
     return {"sentiment": predicted_label.item()}
 
-@app.route("/summurize", methods=["POST"])
-def summurize():
+@app.route("/summarize", methods=["POST"])
+def summarize():
     input_text = request.json.get("text")
 
-    # load the mode
-    model_name = "sshleifer/distilbart-xsum-12-3"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-    # tokenize the input text
-    inputs = tokenizer(input_text, return_tensors="pt")
-
-    # analysis
-    outputs = model(**inputs)
-    logits = outputs.logits
-    predicted_label = torch.argmax(logits)
-
-    return {"summurize": predicted_label.item()}
+    model = BertSummarizer.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    inputs = tokenizer.encode_plus(input_text, return_tensors='pt', max_length=512, truncation=True)
+    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=200)
+    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary_text
 
 
 # if this works. have a way to make python script to download all the models we need
