d3f8912b05b7a0d815ba4652a68e3ad01d802602
jwt2706 <jwt2home@gmail.com>
2024-09-28 04:43:51 -0400

    backend works :)

--- Server/main.py
+++ Server/main.py
@@ -4,29 +4,35 @@ import torch
 
 app = Flask(__name__)
 
-@app.route("/sentiment", methods=["POST"])
-def sentiment():
+goal_categories = ['exercise', 'diet', 'hydration', 'hygiene', 'social', 'sleep', 'production', 'misc']
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
     input_text = request.json.get("text")
-    pipe = pipeline("text-classification")
-    result = pipe(input_text)
-    return result['score']
+    sentences = split_sentences(input_text)
+    classified_sentences = []
+    for sentence in sentences:
+        goal_type = classify_sentence(sentence)
+        goal_status = classify_goal_status(sentence)
+        classified_sentences.append({"goal_type": goal_type, "goal_status": goal_status})
+    return classified_sentences
 
-@app.route("/summarize", methods=["POST"])
-def summarize():
-    input_text = request.json.get("text")
+def split_sentences(text):
+    return text.split(".")
 
-    model = BertSummarizer.from_pretrained('bert-base-uncased')
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    inputs = tokenizer.encode_plus(input_text, return_tensors='pt', max_length=512, truncation=True)
-    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=200)
-    summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary_text
+def classify_sentence(sentence):
+    result = classifier(sentence, goal_categories)
+    print("---------- SENTENCE CLASSIFICTATION START -------------")
+    print(result)
+    print(result['labels'][0])
+    print("---------- SENTENCE CLASSIFICTATION END -------------")
+    return result['labels'][0]
 
-
-# if this works. have a way to make python script to download all the models we need
-# then make this just load the right model from the local
-
-
+def classify_goal_status(sentence):
+    pipe = pipeline("sentiment-analysis")
+    result = pipe(sentence)
+    return result[0]['label']
 
 if __name__ == "__main__":
     app.run(debug=True)
