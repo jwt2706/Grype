9192e3ff31c6400372b375b6b296d69b853632b5
twoadave <gayowsky.d@gmail.com>
2024-09-28 11:26:17 -0400

    usegsap added

--- App/icons_anim_temp.tsx
+++ App/icons_anim_temp.tsx
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { useRef } from 'react';
 
 import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
 
