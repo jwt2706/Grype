561f37df61d6e8698e6af31e7876dfb71146b92d
twoadave <gayowsky.d@gmail.com>
2024-09-28 10:55:34 -0400

    Icons - note temp file can be added to gitignore later

---
+++ App/icons_anim_temp.tsx
@@ -0,0 +1,26 @@
+
+import react from '@vitejs/plugin-react'
+
+// Icon for exercise
+import { BiBody } from "react-icons/bi";
+
+//Icon for diet
+import { BiBowlHot } from "react-icons/bi";
+
+//Icon for hydration
+import { BiDroplet } from "react-icons/bi";
+
+//Icon for hygiene
+import { BiBath } from "react-icons/bi";
+
+//Icon for social
+import { BiCommentError } from "react-icons/bi";
+
+//Icon for sleep
+import { BiBed } from "react-icons/bi";
+
+//Icon for productivity
+import { BiAtom } from "react-icons/bi";
+
+//Icon for misc
+import { BiClipboard } from "react-icons/bi";
