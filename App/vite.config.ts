6a84e0e6cea9d6b5f3c08245eae28cac5d4ec401
jwt2706 <jwt2home@gmail.com>
2024-09-29 04:05:14 -0400

    app base for front end deployment

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/App/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled'],
