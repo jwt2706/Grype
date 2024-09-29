76c3ce1356b0a89c418ad504099c676357ffad7c
batleram <batleram@gmail.com>
2024-09-29 07:29:11 -0400

    minor cleanup

--- App/src/types/EndOfDayWidgetData.ts
+++
@@ -1,9 +0,0 @@
-import { AnimationType } from "./Animation";
-
-export class EndOfDayWidgetData {
-    public animation: AnimationType;
-
-    constructor(_animation: AnimationType) {
-        this.animation = _animation;
-    }
-}
